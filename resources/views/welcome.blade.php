ae2cb89952cf3d9e0a1892e1b522278964642ed5
dennis.wu <dennis.wu@ccdntech.com>
2023-12-26 15:09:00 +0800

    fix.好久沒刻版忘記怎麼刻

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,22 @@
 
         @vite('resources/css/app.css')
     </head>
-    <body class="h-100">
+    <body class="h-100 bg-dark-subtle">
         @include('layout.header')
 
 
         <div class="text-bg-secondary">
+            <form action="" method="post">
+                <div class="input-group flex-nowrap">
+                    <span class="input-group-text">帳號</span>
+                    <input type="text" class="form-control" placeholder="帳號" aria-label="Username">
+                </div>
 
+                <div class="input-group flex-nowrap">
+                    <span class="input-group-text">密碼</span>
+                    <input type="password" class="form-control" placeholder="密碼" aria-label="Username">
+                </div>
+            </form>
         </div>
 
 
