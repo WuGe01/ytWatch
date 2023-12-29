4e777673f887c1ea270dfc5202f77b303232aa7d
dennis.wu <dennis.wu@ccdntech.com>
2023-12-29 14:23:22 +0800

    fix.簡易註冊功能練習

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AccountController extends Controller
 {
@@ -38,6 +41,26 @@ class AccountController extends Controller
 
     public function register(Request $request)
     {
-        return '註冊';
+        $validatedData = Validator::make($request->all(), [
+            'account' => 'required|unique:accounts|max:255',
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:accounts|max:255',
+            'password' => 'required|min:4',
+        ]);
+
+        if ($validatedData->fails()) {
+            return redirect()->back()
+                             ->withErrors($validatedData)
+                             ->withInput();
+        }
+
+        $account = new Account();
+        $account->account = $request->account;
+        $account->name = $request->name;
+        $account->email = $request->email;
+        $account->password = Hash::make($request->password);
+        $account->save();
+
+        return redirect('login');
     }
 }
